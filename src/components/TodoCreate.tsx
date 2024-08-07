64c1f8bf52f2234af03b1de6852ff74dd2d27212
VusalGuliyevv <vsalquliyev@gmail.com>
2024-08-07 23:36:12 +0400

    added icons

--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { FiSun } from "react-icons/fi";
+import { FaMoon } from "react-icons/fa";
 
 function TodoCreate() {
   return (
     <div className="todo-create">
         <input className = "todo-input" type="text" placeholder="Write some note's"/>
         <button className="todo-btn">Click</button>
+        <FiSun style={{fontSize:"25px"}}/>
+        <FaMoon style={{fontSize:"25px"}}/>
     </div>
   )
 }
